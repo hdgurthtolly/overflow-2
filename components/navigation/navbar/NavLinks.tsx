517ff7fec60660bfcd8e6f438c8352c9b6cb6b44
Harry Daniel Gurth Angeles <harry@tolly.io>
2025-05-15 13:57:24 +0200

    Start implementing mobile nav and import necessary components.

---
+++ components/navigation/navbar/NavLinks.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {sidebarLinks.map((item) => {
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+        const LinkComponent = (
+          <Link
+            href={item.route}
+            key={item.label}
+            className={cn(
+              isActive
+                ? "primary-gradient rounded-lg text-light-900"
+                : "text-dark300_light900",
+              "flex items-center justify-start gap-4 bg-transparent p-4"
+            )}
+          >
+            <Image
+              src={item.imgURL}
+              alt={item.label}
+              width={20}
+              height={20}
+              className={cn({ "invert-colors": !isActive })}
+            />
+            <p
+              className={cn(
+                isActive ? "base-bold" : "base-medium",
+                !isMobileNav && "max-lg-hidden"
+              )}
+            >
+              {item.label}
+            </p>
+          </Link>
+        );
+        return LinkComponent;
+      })}
+    </>
+  );
+};
+export default NavLinks;
